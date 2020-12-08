e7c8862c2e8b0489ba39b2d249e0de8a17515bf8
F3nIk <57808667+F3nIk@users.noreply.github.com>
2020-12-08 16:25:16 +0300

    Fixes
    - fixed UI bugs
    - change timer logic

---
+++ Assets/Prototype/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Doozy.Engine.Nody.Models;
+
+[RequireComponent(typeof(Timer))]
+public class LevelManager : MonoBehaviour
+{
+	#region Singleton
+	public static LevelManager Instance;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+	#endregion
+
+	[SerializeField] private Graph ingameGraph;
+	private Timer timer;
+
+	private void OnEnable()
+	{
+		TryGetComponent(out timer);
+		timer.TimeOver += OnTimeOver;
+	}
+
+	private void OnDisable()
+	{
+		timer.TimeOver -= OnTimeOver;
+	}
+
+	private void OnTimeOver()
+	{
+		ingameGraph.SetActiveNodeByName("EndGameEvent");
+
+	}
+	
+
+}
--- Assets/Prototype/Scripts/Timer.cs
+++ Assets/Prototype/Scripts/Timer.cs
@@ -1,31 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
-using UnityEngine.UI;
 using System;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] [Tooltip("Time in seconds")] [Range(0, 300f)] private float Duration = 0;
+    [SerializeField] [Tooltip("Level duration in seconds")] [Range(0, 300f)] private float duration = 0;
+	[SerializeField] [Tooltip("Label to show current timer value (default = null)")] private TextMeshProUGUI label;
 
 	public event UnityAction TimeOver;
 
-	private TextMeshProUGUI label;
 	private float timeLeft;
 
 	private void OnEnable()
 	{
-		timeLeft = Duration;
+		timeLeft = duration;
 	}
 
 	private void OnDisable()
 	{
 		TimeOver?.Invoke();
-	}
-
-	private void Start()
-	{
-		label = GetComponent<TextMeshProUGUI>();
 	}
 
 	private void Update()
@@ -34,7 +28,7 @@ public class Timer : MonoBehaviour
 
 		if (timeLeft <= 0) enabled = false;
 
-		UpdateLabel();
+		if(label != null) UpdateLabel();
 	}
 
 	private void UpdateLabel()
